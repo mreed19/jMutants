567b8d529cae1e02159d6cefba17aaaa8383a221
mreed19 <michael.reed315@gmail.com>
2016-06-07 15:35:06 -0400

    Add form to create mutants

--- js/create-mutants.js
+++ js/create-mutants.js
@@ -1,3 +1,12 @@
+$('#new_mutant').on('submit', function(ev) {
+  ev.preventDefault();
+  createMutant({
+    mutant_name: this.mutantName.value,
+    real_name: this.realName.value,
+    power: this.power.value
+  });
+});
+
 function createMutant(mutant) {
   $.post({
     url: 'https://mutant-school.herokuapp.com/api/v1/mutants',
